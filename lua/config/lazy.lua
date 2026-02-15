042dceb03f4dd58618d8c603dda4791e0f402ccb
thomas mcnutt <thomas.mcnutt.jr@gmail.com>
2026-02-15 16:45:03 -0500

    add window navigation keymaps

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -835,6 +835,12 @@ vim.keymap.set("n", "<leader>c", "<cmd>bdelete<CR>")                            
 vim.keymap.set("n", "<leader>bb", "<cmd>BufferLinePick<CR>")                                   -- Pick which buffer to go to
 vim.keymap.set("n", "<leader>bc", "<cmd>BufferLineCloseLeft<CR><cmd>BufferLineCloseRight<CR>") -- Close all but current
 
+-- Window navigation with Ctrl + hjkl (like tmux)
+vim.keymap.set('n', '<C-h>', '<C-w>h', { desc = 'Move to left window' })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { desc = 'Move to down window' })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { desc = 'Move to up window' })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { desc = 'Move to right window' })
+
 -- git blame
 -- Git blame - show current line blame temporarily
 vim.keymap.set("n", "<leader>gl", function()
