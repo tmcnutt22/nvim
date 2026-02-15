450779d4090a7a586f5af43725272f2d936c519d
thomas mcnutt <thomas.mcnutt.jr@gmail.com>
2026-02-15 16:47:41 -0500

    adjust colorscheme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -858,7 +858,7 @@ require("lazy").setup({
       priority = 1000, -- make sure to load this before all the other start plugins
       config = function()
         require('onedark').setup {
-          style = 'darker'
+          style = 'dark'
         }
         require('onedark').load()
       end
